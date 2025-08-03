a4306447e281916cd766c5f71c175e64a2dc0012
Ran <wwren@Rans-Mac-mini.local>
2025-08-03 16:48:13 +1000

    update weather api

--- src/api/weatherAPI.js
+++ src/api/weatherAPI.js
@@ -5,23 +5,21 @@ const SYDNEY_LON = 150.8508;
 
 export const getWeathers = async function () {
   const data = await fetch(
-    `https://api.openweathermap.org/data/2.5/onecall?lat=${SYDNEY_LAT}5&lon=${SYDNEY_LON}&exclude=hourly&units=metric&appid=${process.env.REACT_APP_WEATHER_API}`
+    `https://api.openweathermap.org/data/3.0/onecall?lat=${SYDNEY_LAT}5&lon=${SYDNEY_LON}&exclude=hourly&units=metric&appid=${process.env.REACT_APP_WEATHER_API}`
   )
     .then((response) => response.json())
     .then((data) => data);
 
   const nextWeekRaw = data.daily;
-  let nextWeekFormat = nextWeekRaw.map(
-    ({ dt, sunrise, sunset, feels_like, wind_speed, weather }) => ({
-      dtRaw: dt,
-      dt: dateReadable(dt),
-      sunrise: timeReadable(sunrise),
-      sunset: timeReadable(sunset),
-      feels_like,
-      wind_speed,
-      weather,
-    })
-  );
+  let nextWeekFormat = nextWeekRaw.map(({ dt, sunrise, sunset, feels_like, wind_speed, weather }) => ({
+    dtRaw: dt,
+    dt: dateReadable(dt),
+    sunrise: timeReadable(sunrise),
+    sunset: timeReadable(sunset),
+    feels_like,
+    wind_speed,
+    weather,
+  }));
   return nextWeekFormat;
 };
 
